f14d5a2046c96c619e8f76e15f7984280b442ded
Jason Tribbeck <jason@tribbeck.com>
2025-04-27 20:30:23 +0100

    Limit maximum Y below land, since there's no collision detection yet. Added start of screen decoration

--- mkimg.bat
+++ mkimg.bat
@@ -1,2 +1,2 @@
-@.\LanderCode\TitleMaker.exe -w 256 -h 16 -s title.data -d title.bin
-@.\LanderCode\TitleMaker.exe -w 256 -h 8 -s baseline.data -d baseline.bin
+@.\LanderCode\net8.0\TitleMaker.exe -w 256 -h 16 -s title.data -d title.bin
+@.\LanderCode\net8.0\TitleMaker.exe -w 256 -h 8 -s baseline.data -d baseline.bin
